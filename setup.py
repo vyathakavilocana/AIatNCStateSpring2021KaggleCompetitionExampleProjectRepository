056748162fc00aed7a7c688a75689617cbf2ae4a
vyathakavilocana <prathammc@gmail.com>
2021-04-13 00:43:18 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Spring 2021 AI at NC State project repository contains all of the machine learning prototyping code for an example Kaggle Competition project. It is a template to use when working on Kaggle competition problems in an orderly fashion befitting of a data scientist.',
+    author='AI at NC State (Pratham Chhabria, Alex Wheelis, Harry Antoniades, Rupin Talwar, Alison Schaefer, Vincent Tompkins, Selene Schmittling) and Clemson AIClub (Jeffrey Wang, David Camp, Jacob Schecter).',
+    license='MIT',
+)
